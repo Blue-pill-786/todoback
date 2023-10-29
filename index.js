b2f7ec2bef2793899e32d1250b2e8d60c65ce266
Blue-pill-786 <113605539+Blue-pill-786@users.noreply.github.com>
2023-10-29 06:14:39 +0530

    reset

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 const express = require("express");
-const cors = require("cors");
-const todoRoutes = require("./src/todoRoutes");
-const notesRoutes = require("./src/notesRoutes")
+const todoRoutes=require("./todoRoutes");
 
 const server = express();
-console.log("Before JSON middleware"); // Add this line
-server.use(express.json()); // This line is your JSON middleware
-console.log("After JSON middleware"); 
-server.use(cors());
-server.use("/api/todos", todoRoutes);
-server.use("/api/notes", notesRoutes)
+server.listen(4100);
 
+server.use("/api/todos/", todoRoutes.router);
 
-// Other routes and server configuration
-// Middleware to parse JSON data from the request body
-
-const PORT = process.env.PORT || 4100;
-server.listen(PORT, () => {
-  console.log(`Server is listening at port ${PORT}`);
+server.get("/", (req, res)=>{
+    res.send("Welcome to Utility api");
 });
+console.log("Server is listening at 4100");
