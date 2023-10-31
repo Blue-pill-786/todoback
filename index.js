1a294b40a79c0dc014430da767f8e381e3db3c3e
Blue-pill-786 <113605539+Blue-pill-786@users.noreply.github.com>
2023-11-01 01:20:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ const notesRoutes = require("./notesRoutes");
 const server = express();
 const port = 4100;
 
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+  });
 server.use("/api/todos", todoRoutes.router);
 server.use("/api/notes/", notesRoutes.notesRouter);
+
 
 server.get("/", (req, res) => {
   res.send("Welcome to Utility API");
