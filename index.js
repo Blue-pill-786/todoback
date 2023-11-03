1cf30e9145d73247f87891c5ee481456d4c713d5
Blue-pill-786 <113605539+Blue-pill-786@users.noreply.github.com>
2023-11-04 03:01:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,11 @@ const cors = require("cors")
 const corsOptions = require('./config/corsOptions')
 const server = express();
 const port = 4100;
-server.use(cors(corsOptions));
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    next();
-  });
+
+server.use(cors(corsOptions)); // Use the cors middleware with your specified options.
+
 server.use("/api/todos", todoRoutes.router);
 server.use("/api/notes/", notesRoutes.notesRouter);
-
 
 server.get("/", (req, res) => {
   res.send("Welcome to Utility API");
