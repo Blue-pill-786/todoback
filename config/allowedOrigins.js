f2dfdc465e3f89a753e2ce1ef923db387b424005
Ubair <113605539+Blue-pill-786@users.noreply.github.com>
2024-05-26 00:34:22 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'https://todo-jfkg.onrender.com',
     'http://localhost:3000',
-    '*'
+    '*',
+    'https://chic-starlight-560ef4.netlify.app'
 ]
 
 module.exports = allowedOrigins
