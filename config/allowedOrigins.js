4a59c46da091b3207abb20ce4d16fe8f2d8faebf
Blue-pill-786 <113605539+Blue-pill-786@users.noreply.github.com>
2024-05-27 02:08:13 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     'https://vermillion-croquembouche-b07cd1.netlify.app/',
     'http://localhost:3000',
     'https://chic-starlight-560ef4.netlify.app',
-    'https://todo-jfkg.onrender.com/'
+    'https://todo-jfkg.onrender.com/',
 ]
 
 module.exports = allowedOrigins
